d7ff585c4e527d5aa02af6fc1a426754c7c21a54
Jyotirmay Barman <jyotirmay@developerzilla.com>
2023-07-17 19:08:23 +0530

    install & configure dependencies related to the js version of the vue-3-starter-js

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,30 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia'
+import { createMetaManager } from 'vue-meta'
 import App from './App.vue'
+import router from './router'
+import { createI18n } from 'vue-i18n'
 
-createApp(App).mount('#app')
+import './assets/index.css'
+
+import en from './locales/en.json'
+import ru from './locales/ru.json'
+
+const i18n = createI18n({
+    legacy: false,
+    locale: 'ru',
+    fallbackLocale: 'en',
+    messages:{
+        en,
+        ru
+    }
+})
+
+const app = createApp(App)
+
+app.use(i18n)
+app.use(createPinia())
+app.use(createMetaManager())
+app.use(router)
+
+app.mount('#app')
